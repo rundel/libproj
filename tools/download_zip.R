15aa4af0f8585c67b8fe725264b122bc757f66b8
Colin Rundel <rundel@gmail.com>
2016-12-18 21:38:45 -0500

    Fix libconf issue?

--- tools/download_zip.R
+++ tools/download_zip.R
@@ -1,8 +1,8 @@
 args = commandArgs(trailingOnly=TRUE)
 
-(url  = args[1])
-(file = args[2])
-(dir  = args[3])
+url  = args[1]
+file = args[2]
+dir  = args[3]
 
 download.file(url, file, quiet = TRUE)
 unzip(file, exdir = dir)
